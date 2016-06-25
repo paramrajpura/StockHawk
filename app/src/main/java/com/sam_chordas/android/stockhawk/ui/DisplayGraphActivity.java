11d1fae3fd3d4eba3a6bc1116d98530a3af3ddab
paramrajpura <param.rajpura92@gmail.com>
2016-06-25 19:18:17 +0530

    1. Fix for RTL Support 2. Candle Stick Chart feature 3. Symbol Suggestion feature

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DisplayGraphActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DisplayGraphActivity.java
@@ -1,18 +1,26 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -39,47 +47,100 @@ import java.util.Date;
 public class DisplayGraphActivity extends AppCompatActivity {
 
     private final String LOG_TAG = "DisplayGraphActivity";
-    private LineChart chart;
-    private ArrayList<Entry> stockPriceList = new ArrayList<>();
-    private ArrayList<String> dateList = new ArrayList<String>();
+    private CandleStickChart chart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
-        String quoteSymbol = intent.getStringExtra(Utils.QUOTE_SYMBOL);
+        final String quoteSymbol = intent.getStringExtra(Utils.QUOTE_SYMBOL);
         setTitle(quoteSymbol);
         FetchHistoricData fetchTask = new FetchHistoricData();
-        fetchTask.execute(quoteSymbol);
+        fetchTask.execute(quoteSymbol, String.valueOf(-1));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_line_graph);
+        final Button monthButton = (Button)findViewById(R.id.monthChart);
+        final Button sixMonthButton = (Button)findViewById(R.id.sixMonthChart);
+        final Button yearButton = (Button)findViewById(R.id.yearChart);
+
+        if (monthButton != null && sixMonthButton!=null && yearButton!=null) {
+            monthButton.setBackgroundColor(getResources().getColor(R.color.material_blue_500));
+            sixMonthButton.setBackgroundColor(Color.BLACK);
+            yearButton.setBackgroundColor(Color.BLACK);
+            monthButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FetchHistoricData fetchTask = new FetchHistoricData();
+                    fetchTask.execute(quoteSymbol, String.valueOf(-1));
+                    monthButton.setBackgroundColor(getResources().getColor(R.color.material_blue_500));
+                    sixMonthButton.setBackgroundColor(Color.BLACK);
+                    yearButton.setBackgroundColor(Color.BLACK);
+                }
+            });
+
+            sixMonthButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FetchHistoricData fetchTask = new FetchHistoricData();
+                    fetchTask.execute(quoteSymbol, String.valueOf(-6));
+                    monthButton.setBackgroundColor(Color.BLACK);
+                    sixMonthButton.setBackgroundColor(getResources().getColor(R.color.material_blue_500));
+                    yearButton.setBackgroundColor(Color.BLACK);
+                }
+            });
+            yearButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FetchHistoricData fetchTask = new FetchHistoricData();
+                    fetchTask.execute(quoteSymbol, String.valueOf(-12));
+                    monthButton.setBackgroundColor(Color.BLACK);
+                    sixMonthButton.setBackgroundColor(Color.BLACK);
+                    yearButton.setBackgroundColor(getResources().getColor(R.color.material_blue_500));
+                }
+            });
+        }
         super.onCreate(savedInstanceState);
     }
 
     public class FetchHistoricData extends AsyncTask<String, Void, Void> {
+        private ArrayList<CandleEntry> stockPriceList = new ArrayList<>();
+        private ArrayList<String> dateList = new ArrayList<String>();
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            chart = (LineChart) findViewById(R.id.DailyChart);
-            LineDataSet priceData = new LineDataSet(stockPriceList,
+            chart = (CandleStickChart) findViewById(R.id.DailyChart);
+            CandleDataSet priceData = new CandleDataSet(stockPriceList,
                     getString(R.string.desc_graph));
-            priceData.setDrawCircles(true);
             priceData.setDrawValues(false);
-            LineData graphData = new LineData(dateList,priceData);
+            CandleData graphData = new CandleData(dateList,priceData);
             if (chart != null) {
+                priceData.setAxisDependency(YAxis.AxisDependency.LEFT);
+                priceData.setShadowColor(Color.WHITE);
+                priceData.setShadowWidth(0.7f);
+                priceData.setDecreasingColor(Color.RED);
+                priceData.setDecreasingPaintStyle(Paint.Style.FILL);
+                priceData.setIncreasingColor(Color.rgb(122, 242, 84));
+                priceData.setIncreasingPaintStyle(Paint.Style.FILL);
+                priceData.setNeutralColor(Color.BLUE);
                 chart.getAxisRight().setEnabled(false);
                 chart.setData(graphData);
                 chart.setDescription("");
+                //chart.setAutoScaleMinMaxEnabled(true);
+                //chart.setKeepPositionOnRotation(true);
                 Legend l = chart.getLegend();
                 l.setTextColor(Color.WHITE);
 
                 YAxis y = chart.getAxisLeft();
                 y.setTextColor(Color.WHITE);
                 y.setDrawLimitLinesBehindData(false);
-                y.setDrawGridLines(false);
+                y.setDrawGridLines(true);
+                y.setGridColor(Color.DKGRAY);
 
                 XAxis x = chart.getXAxis();
                 x.setTextColor(Color.WHITE);
                 x.setPosition(XAxis.XAxisPosition.BOTTOM);
-
+                x.setDrawGridLines(true);
+                x.setGridColor(Color.DKGRAY);
+                chart.notifyDataSetChanged();
                 chart.invalidate();
             }
             super.onPostExecute(aVoid);
@@ -107,7 +168,7 @@ public class DisplayGraphActivity extends AppCompatActivity {
 
             Calendar c = Calendar.getInstance();
             c.setTime(endDate);
-            c.add(Calendar.MONTH, -1);
+            c.add(Calendar.MONTH, Integer.parseInt(params[1]));
             Date startDate = c.getTime();
             String fStartDate = format.format(startDate);
             buildURL.appendQueryParameter(QUERY_KEY,
@@ -119,7 +180,6 @@ public class DisplayGraphActivity extends AppCompatActivity {
                     .appendQueryParameter(ENV_KEY, ENV_VALUE)
                     .appendQueryParameter(CALLBACK_KEY, CALLBACK_VALUE).build();
             String quoteHistoricData = urlConnectFetchData(buildURL.build().toString());
-
             try {
                 JSONObject jsonObj1 = new JSONObject(quoteHistoricData);
                 JSONObject jsonObj2 = jsonObj1.getJSONObject("query").getJSONObject("results");
@@ -128,8 +188,13 @@ public class DisplayGraphActivity extends AppCompatActivity {
                     JSONObject dailyData = jsonArray.getJSONObject(i);
                     String date = dailyData.getString("Date");
                     dateList.add(date);
-                    stockPriceList.add(new Entry(Float.parseFloat(
-                            dailyData.getString("Adj_Close")),jsonArray.length()-i-1));
+                    float openPrice = Float.parseFloat(dailyData.getString("Open"));
+                    float highPrice = Float.parseFloat(dailyData.getString("High"));
+                    float lowPrice = Float.parseFloat(dailyData.getString("Low"));
+                    float closePrice = Float.parseFloat(dailyData.getString("Close"));
+
+                    stockPriceList.add(new CandleEntry(jsonArray.length()-i-1,
+                            highPrice,lowPrice,openPrice,closePrice));
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
